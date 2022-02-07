f86d709ee989184d693b081fbf629038c7e92851
Martín <magadiflo@gmail.com>
2022-02-07 12:58:56 -0500

    Obtener tiempo transcurrido y restante

--- src/app/shared/services/multimedia.service.ts
+++ src/app/shared/services/multimedia.service.ts
@@ -9,6 +9,8 @@ export class MultimediaService {
 
   callback: EventEmitter<any> = new EventEmitter<any>();
   public trackInfo$: BehaviorSubject<any> = new BehaviorSubject(undefined);
+  public timeElapsed$: BehaviorSubject<string> = new BehaviorSubject('00:00');
+  public timeRemaining$: BehaviorSubject<string> = new BehaviorSubject('-00:00');
   public audio!: HTMLAudioElement;
 
   constructor() {
@@ -18,12 +20,47 @@ export class MultimediaService {
         this.setAudio(res);
       }
     });
+
+    this.listenAllEvents();
   }
 
   public setAudio(track: TrackModel): void {
     console.log('setAudio(...)', track);
     this.audio.src = track.url;
     this.audio.play();
+  }
+
+  private listenAllEvents(): void {
+    this.audio.addEventListener('timeupdate', this.calculateTime, false);
+  }
+
+  private calculateTime = (): void => {
+    console.log('Disparando evento');
+    const { duration, currentTime } = this.audio;
+    console.table([duration, currentTime]);
+    this.setTimeElapsed(currentTime);
+    this.setTimeRemaining(currentTime, duration);
+  }
+
+  private setTimeElapsed(currentTime: number): void {
+    const displayFormat = this.getTimeFormat(currentTime, '');
+    this.timeElapsed$.next(displayFormat);
+  }
+
+  private setTimeRemaining(currentTime: number, duration: number): void {
+    let timeLeft = duration - currentTime;
+    const displayFormat = this.getTimeFormat(timeLeft, '-');
+    this.timeRemaining$.next(displayFormat);
+  }
+
+  private getTimeFormat(time: number, symbol: string): string {
+    let seconds = Math.floor(time % 60);
+    let minutes = Math.floor((time / 60) % 60);
+
+    const displaySeconds = (seconds < 10) ? `0${seconds}` : seconds;
+    const displayMinutes = (minutes < 10) ? `0${minutes}` : minutes;
+
+    return `${symbol}${displayMinutes}:${displaySeconds}`;
   }
 
 }
