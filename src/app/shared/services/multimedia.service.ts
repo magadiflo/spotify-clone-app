2fd4aa3eaac34cf16d57a9042d7707f9acae50b8
Martín <magadiflo@gmail.com>
2022-02-07 11:17:57 -0500

    Rxjs Subject y BehaviorSubject

--- src/app/shared/services/multimedia.service.ts
+++ src/app/shared/services/multimedia.service.ts
@@ -1,5 +1,5 @@
 import { EventEmitter, Injectable } from '@angular/core';
-import { Observable, Observer } from 'rxjs';
+import { BehaviorSubject, Observable, Observer, Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -7,14 +7,33 @@ import { Observable, Observer } from 'rxjs';
 export class MultimediaService {
 
   callback: EventEmitter<any> = new EventEmitter<any>();
-  myObservable1$: Observable<any> = new Observable();
+  // myObservable1$: Observable<any> = new Observable();
+  // myObservable1$: Subject<any> = new Subject();
+  myObservable1$: BehaviorSubject<any> = new BehaviorSubject('Inicializado con agua...');
+// 
 
   constructor() {
-    this.myObservable1$ = new Observable((observer: Observer<any>) => {
-      observer.next('Sí, agua...');
-      observer.complete();
-      observer.error('Hubo un error papi...');
-    });
+    /*********** Observable **************** */
+    // this.myObservable1$ = new Observable((observer: Observer<any>) => {
+    //   observer.next('Sí, agua...');
+    //   observer.complete();
+    //   observer.error('Hubo un error papi...');
+    // });
+
+    //************* Subject y BehaviorSubject **************** */
+    setTimeout(() => {
+      this.myObservable1$.next('enviandoo agua desde multimedia...');
+    }, 1000);
+
+    setTimeout(() => {
+      this.myObservable1$.error('set tapó la tubería');
+    }, 2000);
+
+    setTimeout(() => {
+      this.myObservable1$.complete();
+    }, 3000);
+
+    
   }
 
 }
